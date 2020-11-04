4ec6106d785176dcab830831a3a77710efcbd9a5
Knut Andreas Meyer <knutam@gmail.com>
2020-11-05 00:02:38 +0100

    Added support for finding in double array using a tolerance

--- src/find_mod.f90
+++ src/find_mod.f90
@@ -6,7 +6,7 @@ private
 public  :: find                 ! Find position in an array being equal to a given value
 
 interface find
-    procedure find_int_1d, find_int_2d
+    procedure find_int_1d, find_int_2d, find_dbl_1d_tol, find_dbl_2d_tol
 end interface find
 
 
@@ -37,5 +37,33 @@ implicit none
     if (abs(array(ind(1), ind(2))-val_to_find) /= 0) ind = -1
     
 end function find_int_2d
+
+function find_dbl_1d_tol(array, val_to_find, tol) result(ind)
+implicit none
+    double precision, intent(in)    :: array(:,:)
+    double precision, intent(in)    :: val_to_find
+    double precision, intent(in)    :: tol
+    integer                         :: ind, tmp_ind(1)
+    
+    tmp_ind = minloc(abs(array - val_to_find))
+    ind = tmp_ind(1)
+    
+    if (abs(array(ind)-val_to_find) > tol) ind = -1
+    
+end function find_dbl_1d_tol
+
+function find_dbl_2d_tol(array, val_to_find, tol) result(ind)
+implicit none
+    double precision, intent(in)    :: array(:,:)
+    double precision, intent(in)    :: val_to_find
+    double precision, intent(in)    :: tol
+    integer                         :: ind(2)
+    
+    ind = minloc(abs(array - val_to_find))
+    
+    if (abs(array(ind(1), ind(2))-val_to_find) > tol) ind = -1
+    
+end function find_dbl_2d_tol
+
 
 end module
